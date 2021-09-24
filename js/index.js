346919dff19d549cb31691d82a634ecfaa916dd5
Kaluzy <88167819+Kaluzy@users.noreply.github.com>
2021-09-24 17:07:36 -0400

    Finished with Task-5

--- js/index.js
+++ js/index.js
@@ -8,29 +8,17 @@ const validForm = (e) => {
   const newTaskAssign = document.querySelector("#assign");
   const newTaskdate = document.querySelector("#task-due-date");
   //The value the form
-  const tasks = newTaskName.value;
+  let tasks = newTaskName.value;
   const descriptions = newTaskDesc.value;
   const assignTo = newTaskAssign.value;
   const dueDate = newTaskdate.value;
-  //console.log(name);
-  // console.log(description);
-  // console.log(assignTo);
-  // console.log(dueDate);
-  let newForm = new TaskManager();
-  newForm.addTask(tasks, descriptions, assignTo, dueDate, "pending");
   alertMessage();
-  console.log(newForm.tasks[0]);
-  // const newTask = new TaskManager();
-  // newTask.addTask(
-  //   name,
-  //   description,
-  //   assignTo,
-  //   dueDate
-  //   //"TODO"
-  // );
-  // console.log(newTask.tasks);
-};
+  //tasks.TaskManager.addTask();
+  // function clear() {
+  // }
 
+  console.log(tasks);
+};
 const alertMessage = () => {
   let name = document.querySelector("#task").value;
   let description = document.querySelector("#task-description").value;
@@ -47,50 +35,6 @@ const alertMessage = () => {
   } else {
     window.alert("Your task has been submitted successfully");
   }
+  document.taskform.reset();
+  //clear();
 };
-
-// console.log(newTask);
-
-// let newTask2 = new TaskManager();
-// newTask.addTask(
-//   "task 2",
-//   "task description 2",
-//   "Raven++",
-//   "2020-09-25"
-//   //"TODO"
-// );
-
-// console.log(newTask);
-
-// let newTask2 = new TaskManager();
-// newTask2.addTask(
-//   "new task2",
-//   "new task description",
-//   "Raven",
-//   "2020-09-25",
-//   "TODO"
-// );
-// console.log(newTask2.currentId, newTask2.tasks);
-
-//let newAddTask = new newAddTask()
-/*newAddTask = {
-  currentId: 0,
-  tasks: "new task added",
-  descriptions: "Complete Task 1 - 4",
-  assignTo: "Raven",
-  dueDate: "2020-09-20",
-  status: "",
-};*/
-
-// newTask = {
-//   id: 0,
-//   tasks: "Complete and test project for Sprint 1",
-// descriptions: "Complete Task 1 - 4",
-// assignTo: "Raven",
-// dueDate: "2020-09-20",
-// status: "TODO",
-// };
-// // newTask.TaskManager();
-// console.log(newTask);
-
-// document.getElementById("submit").addEventListener("click");
